6c698af6dac5e64c3c6f2dd32eeb7d7db0c81e68
Fadri-Hidayat <103362054+Fadri-Hidayat@users.noreply.github.com>
2023-05-04 21:09:38 +0700

    Add files via upload

---
+++ pertemuan 5/Latihan5/Latihan5/Latihan5/Form3.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.txtKodeJabatan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(198, 49)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(155, 17)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "Latihan 5 Nilai Akhir"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(58, 248)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(243, 17)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Fadri Hidayat_200511060-TI20C"
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.Location = New System.Drawing.Point(61, 185)
+        Me.txtKeterangan.Multiline = True
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(458, 35)
+        Me.txtKeterangan.TabIndex = 18
+        '
+        'txtKodeJabatan
+        '
+        Me.txtKodeJabatan.Location = New System.Drawing.Point(61, 102)
+        Me.txtKodeJabatan.Multiline = True
+        Me.txtKodeJabatan.Name = "txtKodeJabatan"
+        Me.txtKodeJabatan.Size = New System.Drawing.Size(272, 34)
+        Me.txtKodeJabatan.TabIndex = 17
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(58, 151)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 17)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Keterangan"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(58, 73)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(92, 17)
+        Me.label1.TabIndex = 15
+        Me.label1.Text = "Kode jabatan"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(365, 102)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(164, 34)
+        Me.btnSubmit.TabIndex = 14
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(587, 314)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.txtKodeJabatan)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents txtKodeJabatan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents label1 As Label
+    Friend WithEvents btnSubmit As Button
+End Class
