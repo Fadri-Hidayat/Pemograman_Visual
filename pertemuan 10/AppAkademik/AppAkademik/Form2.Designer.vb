1c66bead84f31a8999dd0150b0a92779ac196fd1
Fadri-Hidayat <103362054+Fadri-Hidayat@users.noreply.github.com>
2023-07-20 18:49:54 +0700

    Add files via upload

---
+++ pertemuan 10/AppAkademik/AppAkademik/Form2.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSKS = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNamaMK = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtKodeMK = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnHapus
+        '
+        Me.btnHapus.BackColor = System.Drawing.Color.Red
+        Me.btnHapus.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHapus.Location = New System.Drawing.Point(411, 105)
+        Me.btnHapus.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(100, 26)
+        Me.btnHapus.TabIndex = 33
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = False
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.btnSimpan.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSimpan.Location = New System.Drawing.Point(411, 38)
+        Me.btnSimpan.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(100, 26)
+        Me.btnSimpan.TabIndex = 32
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Gold
+        Me.btnReset.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(411, 71)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(100, 26)
+        Me.btnReset.TabIndex = 31
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(118, 131)
+        Me.txtProdi.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(132, 23)
+        Me.txtProdi.TabIndex = 30
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(18, 131)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 16)
+        Me.Label4.TabIndex = 29
+        Me.Label4.Text = "Prodi"
+        '
+        'txtSKS
+        '
+        Me.txtSKS.Location = New System.Drawing.Point(118, 101)
+        Me.txtSKS.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSKS.Name = "txtSKS"
+        Me.txtSKS.Size = New System.Drawing.Size(132, 23)
+        Me.txtSKS.TabIndex = 28
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(18, 101)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 16)
+        Me.Label3.TabIndex = 27
+        Me.Label3.Text = "SKS"
+        '
+        'txtNamaMK
+        '
+        Me.txtNamaMK.Location = New System.Drawing.Point(118, 71)
+        Me.txtNamaMK.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNamaMK.Name = "txtNamaMK"
+        Me.txtNamaMK.Size = New System.Drawing.Size(132, 23)
+        Me.txtNamaMK.TabIndex = 26
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 71)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 16)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Nama MK"
+        '
+        'txtKodeMK
+        '
+        Me.txtKodeMK.Location = New System.Drawing.Point(118, 41)
+        Me.txtKodeMK.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtKodeMK.Name = "txtKodeMK"
+        Me.txtKodeMK.Size = New System.Drawing.Size(132, 23)
+        Me.txtKodeMK.TabIndex = 24
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 41)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 16)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Kode MK"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 161)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(508, 175)
+        Me.DataGridView1.TabIndex = 22
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("News706 BT", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(125, 350)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(285, 20)
+        Me.Label5.TabIndex = 34
+        Me.Label5.Text = "Fadri Hidayat_200511060_TIF20C"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(528, 382)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtSKS)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNamaMK)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtKodeMK)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Font = New System.Drawing.Font("News706 BT", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSKS As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNamaMK As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtKodeMK As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label5 As Label
+End Class
